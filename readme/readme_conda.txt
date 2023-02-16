ad32b19cdf4e1f36d69179f17649cba4b4724464
Suraphop <suraphop.bunsawat@gmail.com>
2023-02-16 12:42:01 +0700

    init

--- readme/readme_conda.txt
+++ readme/readme_conda.txt
@@ -3,3 +3,10 @@
 conda create -n basic_python python
 conda activate basic_python
 conda deactivate
+
+
+pip install ipykernel
+pip uninstall ipykernel
+
+conda env remove -n test2
+
---
+++ test.py
@@ -0,0 +1,20 @@
+# %% [markdown]
+# # Hello world
+# ## Hello world 2
+# ### Hello world 3
+# #### Hello world 3
+# hello world 4
+# 
+# - bullet1
+# - bullet2
+#   - bullet2-1
+#   - bullet2-2
+
+# %%
+print("hello world")
+print("2")
+
+# %%
+print("cell 2-1")
+
+
