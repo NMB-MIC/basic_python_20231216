fbca482008cd55fb5c1a40586b71ef050eec5608
Suraphop <suraphop.bunsawat@gmail.com>
2023-02-22 10:13:48 +0700

    init

--- main.bat
+++ main.bat
@@ -1,1 +1,1 @@
-conda activate mms && python E:\MCSTATUS\mcstatus.py
+conda activate basic_python && python "D:/My Documents/Desktop/New folder (2)/basic_python_20231216/main.py"
--- main2.bat
+++
@@ -1,1 +0,0 @@
-conda activate basic_python && python D:/My Documents/Desktop/New folder (2)/basic_python_20231216/main.py
